53d44cb2dece0ac919dce33dfeb54910f44f9a1a
Gates_ice <gates@gatesice.com>
2014-09-15 13:59:40 -0500

    Program lock modified.

--- pet.py
+++ pet.py
@@ -2,31 +2,35 @@ from TaskManager import TaskManager
 from db import MongoDataAdapter
 from time import sleep
 import daemon
+import lockfile
 
-def main():
-    # TODO Load config.
-    mongo = MongoDataAdapter.create_adapter('GatesPet')
-    max_tasks = 20
-    scan_interval = 5
-    process_interval = 120
-    
-    # TaskManager instance.
-    tm = TaskManager(mongo, max_tasks=max_tasks, scan_interval=scan_interval)
-    
-    # Fix tasks that previous instance did not proceed.
-    mongo.fix_tasks()
-    
-    # Main loop
-    tm.start()
-    
-    while True:
-        tasks = mongo.get_scheduled_tasks()
-        for task in tasks:
-            tm.queue_task(task['module'], task['_id'], task['configure'], True)
-            print "[pet] Task %s added." % task['_id']
-            
-        sleep(process_interval)
+program_lock = lockfile.LockFile('pet.lock')
+if program_lock.is_locked():
+    print 'Another instance is running. Exit'
+    exit()
+
+program_lock.acquire()
+
+# TODO Load config.
+mongo = MongoDataAdapter.create_adapter('GatesPet')
+max_tasks = 20
+scan_interval = 5
+process_interval = 120
+
+# TaskManager instance.
+tm = TaskManager(mongo, max_tasks=max_tasks, scan_interval=scan_interval)
+
+# Fix tasks that previous instance did not proceed.
+mongo.fix_tasks()
+
+# Main loop
+tm.start()
+
+while True:
+    tasks = mongo.get_scheduled_tasks()
+    for task in tasks:
+        tm.queue_task(task['module'], task['_id'], task['configure'], True)
+        print "[pet] Task %s added." % task['_id']
         
+    sleep(process_interval)
         
-with daemon.DaemonContext():
-    main()
