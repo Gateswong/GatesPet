f51a561dbe23fdc7fb7729e4e777ddf54561308c
Gates_ice <gates@gatesice.com>
2014-09-15 10:16:17 -0500

    Daemonized main loop.

--- pet.py
+++ pet.py
@@ -1,28 +1,32 @@
 from TaskManager import TaskManager
 from db import MongoDataAdapter
 from time import sleep
+import daemon
 
-# TODO Load config.
-mongo = MongoDataAdapter.create_adapter('GatesPet')
-max_tasks = 20
-scan_interval = 5
-process_interval = 120
-
-# TaskManager instance.
-tm = TaskManager(mongo, max_tasks=max_tasks, scan_interval=scan_interval)
-
-# Fix tasks that previous instance did not proceed.
-mongo.fix_tasks()
-
-# Main loop
-tm.start()
-
-while True:
-    tasks = mongo.get_scheduled_tasks()
-    for task in tasks:
-        tm.queue_task(task['module'], task['_id'], task['configure'], True)
-        print "[pet] Task %s added." % task['_id']
+def main():
+    # TODO Load config.
+    mongo = MongoDataAdapter.create_adapter('GatesPet')
+    max_tasks = 20
+    scan_interval = 5
+    process_interval = 120
+    
+    # TaskManager instance.
+    tm = TaskManager(mongo, max_tasks=max_tasks, scan_interval=scan_interval)
+    
+    # Fix tasks that previous instance did not proceed.
+    mongo.fix_tasks()
+    
+    # Main loop
+    tm.start()
+    
+    while True:
+        tasks = mongo.get_scheduled_tasks()
+        for task in tasks:
+            tm.queue_task(task['module'], task['_id'], task['configure'], True)
+            print "[pet] Task %s added." % task['_id']
+            
+        sleep(process_interval)
         
-    sleep(process_interval)
-    
-    
+        
+with daemon.DaemonContext():
+    main()
